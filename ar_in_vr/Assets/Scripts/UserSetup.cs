98bb8c2ef5b7319cf22e8811ad22f47c153a85bc
Bruce Weir <bruce.weir@bbc.co.uk>
2021-03-08 16:33:32 +0000

    User position can now be tuned at runtime using WASD QE keys

---
+++ ar_in_vr/Assets/Scripts/UserSetup.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UserSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject cameraOffset;
+    public float step = 0.05f;
+
+    Transform mainCameraTransform;
+
+    void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Recentre(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Recentering");
+
+        if (ctx.started)
+        {
+            cameraOffset.transform.localPosition = new Vector3(-mainCameraTransform.localPosition.x, -mainCameraTransform.localPosition.y, -mainCameraTransform.localPosition.z);
+        }
+    }
+
+    public void MoveForward(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            cameraOffset.transform.localPosition += new Vector3(0, 0, step);
+        }
+    }
+
+    public void MoveBack(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            cameraOffset.transform.localPosition -= new Vector3(0, 0, step);
+        }
+    }
+
+    public void MoveLeft(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            cameraOffset.transform.localPosition -= new Vector3(step, 0, 0);
+        }
+    }
+
+    public void MoveRight(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            cameraOffset.transform.localPosition += new Vector3(step, 0, 0);
+        }
+    }
+
+    public void MoveUp(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            cameraOffset.transform.localPosition += new Vector3(0, step, 0);
+        }
+    }
+
+    public void MoveDown(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            cameraOffset.transform.localPosition -= new Vector3(0, step, 0);
+        }
+    }
+}
