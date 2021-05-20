d516159e766ed03f4b6daf422468576854997ed5
pejmansaeghe <pejman.saeghe@manchester.ac.uk>
2021-05-20 12:14:02 +0100

    sea turtle added with basic layered animation. swims and moves towards viewer.

---
+++ ar_in_vr/Assets/Pejman/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GameController : MonoBehaviour
+{
+    /// <summary>
+    /// control turtle's animation based on the video
+    /// </summary>
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private GameObject turtle;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = turtle.GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(videoPlayer.frame);
+        if(videoPlayer.frame > 50)
+        {
+            animator.SetBool("IsSwimming", true);
+            animator.SetBool("IsMovingForward", true);
+        }
+    }
+
+    public void StopMovingForward()
+    {
+        animator.SetBool("IsMovingForward", false);
+        Debug.Log("Stop Moving Towards Viewer");
+    }
+}
---
+++ ar_in_vr/Assets/moveTowardsViewer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTowardsViewer : MonoBehaviour
+{
+    SeaTurtleCharacter turtle;
+
+    private void Awake()
+    {
+        turtle = GetComponent<SeaTurtleCharacter>();
+    }
+
+    private void Start()
+    {
+        turtle.forwardSpeed = 2;
+        turtle.Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
